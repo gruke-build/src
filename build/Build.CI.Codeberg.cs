297b5f8bc870d1e9dd22d26e7c56deb65bc22c3c
GreemDev <greemdev@ryujinx.app>
2026-03-16 03:14:46 -0500

    switch back to medium (non-lazy) to see if that's the cause of the new CI failures (I haven't changed anything else)

--- build/Build.CI.Codeberg.cs
+++ build/Build.CI.Codeberg.cs
@@ -7,7 +7,7 @@ using Nuke.Components;
 
 [ForgejoActions(
     "forgejo-integration-test",
-    CodebergRunners.MediumLazy,
+    CodebergRunners.Medium,
     FetchDepth = 0,
     OnPushBranchesIgnore = [MasterBranch, $"{ReleaseBranchPrefix}/*"],
     OnPullRequestBranches = [DevelopBranch],
