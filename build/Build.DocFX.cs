ea8f0ecd4af717c3b8607c73d20488242b08abcb
GreemDev <greemdev@ryujinx.app>
2026-03-18 02:33:35 -0500

    replace manual CI logic with NUKE code for docfx

---
+++ build/Build.DocFX.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2026 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/gruke-build/src/blob/master/LICENSE
+
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tools.DocFX;
+using Nuke.Common.Tools.Git;
+using Nuke.Components;
+
+public partial class Build
+{
+    AbsolutePath DocFxConfiguration => RootDirectory / "docs" / "docfx.json";
+    AbsolutePath ChangelogOutput => RootDirectory / "docs" / "changelog.md";
+
+    Target DocFx => _ => _
+        .Requires(() => GitTasks.GitHasCleanWorkingCopy())
+        .Executes(() =>
+        {
+            AbsolutePath.Create(From<IHazChangelog>().ChangelogFile)
+                .Copy(ChangelogOutput, ExistsPolicy.FileOverwrite, createDirectories: true);
+            try
+            {
+                DocFXTasks.DocFX($"{DocFxConfiguration}");
+            }
+            finally
+            {
+                ChangelogOutput.DeleteFile();
+            }
+        });
+}
