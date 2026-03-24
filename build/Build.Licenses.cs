55b65914877b7ca6218c968a830fd6e13ff40ef0
GreemDev <greemdev@ryujinx.app>
2026-03-24 01:53:37 -0500

    remove Microsoft.ApplicationInsights license (I removed telemetry)

--- build/Build.Licenses.cs
+++ build/Build.Licenses.cs
@@ -22,7 +22,6 @@ partial class Build
             ("Glob", "https://raw.githubusercontent.com/kthompson/glob/develop/LICENSE"),
                ("ICSharpCode.SharpZipLib", "https://raw.githubusercontent.com/icsharpcode/SharpZipLib/master/LICENSE.txt"),
                ("JetBrains.Annotations", "https://raw.githubusercontent.com/JetBrains/JetBrains.Annotations/main/license.md"),
-               ("Microsoft.ApplicationInsights", "https://raw.githubusercontent.com/microsoft/ApplicationInsights-dotnet/main/LICENSE"),
                ("Microsoft.Build", "https://raw.githubusercontent.com/dotnet/msbuild/main/LICENSE"),
                ("Microsoft.CodeAnalysis", "https://raw.githubusercontent.com/dotnet/roslyn/main/License.txt"),
                ("Newtonsoft.Json", "https://raw.githubusercontent.com/JamesNK/Newtonsoft.Json/master/LICENSE.md"),
